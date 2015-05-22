8c1e244140eb5ff7041c1b69c2c39edc083a5e7b
dykim <Home@dykim-macbook-air.local>
2015-05-22 11:48:33 +0900

    345

--- simpleSwift3/ViewController.swift
+++ simpleSwift3/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    //no problem
+    //no problem 5
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

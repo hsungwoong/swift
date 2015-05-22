89875f445e81d0f2822a5241f87f0a5a204b110f
hsungwoong <sw216@naver.com>
2015-05-22 11:05:51 +0900

    Initial Commit

---
+++ simpleSwift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleSwift3
+//
+//  Created by 성웅 황 on 2015. 5. 22..
+//  Copyright (c) 2015년 cstone dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
